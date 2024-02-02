f2b88d9db4c6b7592cac5d395cd5af67fb88ca09
DogeOfFreedom <jchensi2000@gmail.com>
2024-02-02 21:47:53 +1100

    feat: positioned button in top right corner of content box

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,26 +2,28 @@ import "../style.css"
 import { createProject, addToDo, deleteToDo } from "./project.js";
 import { createToDo, changePriority } from "./todo.js";
 
-let printOut = todos => {
-    for(let todo of todos) {
-        console.log(todo);
-    }
-}
 
-let title = "Task 1";
-let description = "Lorem Ipsum is my babe";
-let dueDate = "pog pog pog";
-let priority = "high";
 
-let project1 = createProject("myProject");
-let todo1 = createToDo(title, description, dueDate, priority);
-let todo2 = createToDo("bitch", description, dueDate, priority);
-let todo3 = createToDo("bitchesg", description, dueDate, priority);
-addToDo(project1, todo1);
-addToDo(project1, todo2);
-addToDo(project1, todo3);
-printOut(project1.todos);
-deleteToDo(project1, todo2);
-console.log("new");
-printOut(project1.todos);
+// let printOut = todos => {
+//     for(let todo of todos) {
+//         console.log(todo);
+//     }
+// }
+
+// let title = "Task 1";
+// let description = "Lorem Ipsum is my babe";
+// let dueDate = "pog pog pog";
+// let priority = "high";
+
+// let project1 = createProject("myProject");
+// let todo1 = createToDo(title, description, dueDate, priority);
+// let todo2 = createToDo("bitch", description, dueDate, priority);
+// let todo3 = createToDo("bitchesg", description, dueDate, priority);
+// addToDo(project1, todo1);
+// addToDo(project1, todo2);
+// addToDo(project1, todo3);
+// printOut(project1.todos);
+// deleteToDo(project1, todo2);
+// console.log("new");
+// printOut(project1.todos);
 
